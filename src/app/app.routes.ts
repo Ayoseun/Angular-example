d58a11ea85534bafb79ad678e8e1854838da7a7b
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2024-09-19 21:10:21 +0100

    .

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-
-export const routes: Routes = [
-  { path: '', component: AppComponent }, // Home or default route
-  { path: 'checkout', component: CheckoutComponent }, // Checkout route
-];
